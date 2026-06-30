5535c1fb633f287220e9ebe4bbaaf4ed271579a9
Marco Antonio - The Way of the Artisan <markitos.es.info@gmail.com>
2026-07-01 00:14:39 +0200

    feat: event name as param

--- cmd/app/demo/main.go
+++ cmd/app/demo/main.go
@@ -12,7 +12,7 @@ func main() {
 func runClient() {
 	dbPath := "../mdk-event-relay/events.db"
 	client := client.NewEventRelayClient(dbPath)
-	client.Publish(`{
+	client.Publish("user-have-been-registered", `{
 		"name": "markitos",
 		"date": "any",
 		"send": true
